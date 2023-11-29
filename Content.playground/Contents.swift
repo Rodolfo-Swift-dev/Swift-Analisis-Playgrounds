9ae1466616482241f4e8c6e91a730ed20e95d120
rodoolfogonzalez <rodolfo.swift.dev@gmail.com>
2023-11-29 08:39:17 -0300

    update

--- Content.playground/Contents.swift
+++ Content.playground/Contents.swift
@@ -5,62 +5,88 @@ import Foundation
 
 //0-Basic Playground
 
-//-
-//-
+//-Comentarios
+//-Operadores
+//-Tipos basicos de datos
+//-Inferencia
+//-Interpolacion y concatenacion
+//-Variables y constantes
 
 
 //1-TypeAlias
 
-//-
-//-
+//-Caracteristicas
 
 
 //2-Tuplas
 
-//-
-//-
+//-Acceder a datos de tuplas
+//-Extraer datos de una tupla
+//-Tuplas con tuplas en su interior
 
 
 //3-Optionals
 
-//-
-//-
+//-Creacion
+//-Enlace opcional If Let
+//-Desenvolvimiento forzado
+//-Operador de fusion nula
+//-Comprobacion de valores nulos
+//-Encadenamiento opcional
+//-Guard let
 
 
 //4-Colecciones
 
-//-
-//-
+//-Arreglo
+//-Sets
+//-Diccionario
 
 
 //5-Control Flow
 
-//-
-//-
+//-If, Else y Else If
+//-Guard
+//-Switch
+//-For
+//-While
+//Repeat While
 
 
 //6-Funciones
 
-//-
-//-
+//-Caracteristicas
+//-Tipo
+//-Funcion dentro de variables
+//-Funcion con retorno de tuplas
+//-Funciones con parametros y sin parametros
+//-Funcion dentro de variables
+//-Funcion con mksmo nombre
+//-Funcion con parametros con valores por defecto
+//-Parametros Variadic
+//-Parametros inout
+//-Funciones dentro de funciones
 
 
 //7-Clases y Estructuras
 
-//-
-//-
+//-Caracteristicas
+//-Creacion
+//-Instancia
 
 
 //8-Metodos
 
-//-
-//-
+//-Metodos de instancia
+//-Metodos de tipo
 
 
 //9-Propiedades
 
-//-
-//-
+//-Propiedades de instancia
+//-Propiedades de tipo
+//-Propiedades calculadas
+//-Property wrapper
 
 
 //10-Closure
